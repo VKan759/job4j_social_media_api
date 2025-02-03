88850627a0569edcf7086eeb2453ee757ddaac13
vyacheslav <slava@paygate.kz>
2025-02-03 09:15:50 +0500

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
@@ -3,6 +3,5 @@ package ru.job4j.socialmedia.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.job4j.socialmedia.model.User;
 
-
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
