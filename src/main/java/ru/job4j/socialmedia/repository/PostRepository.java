3dbb495ed2ce0c42941ef686d6c17dc0ee9ff307
vyacheslav <slava@paygate.kz>
2025-02-06 08:49:00 +0500

    HQL, JPQL, Native SQL [#505064]

--- src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
@@ -1,18 +1,50 @@
 package ru.job4j.socialmedia.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.socialmedia.model.Post;
 import ru.job4j.socialmedia.model.User;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Transactional
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findAllByUser(User user);
 
     List<Post> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 
     Page<Post> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            update Post p set p.header = :fHeader, p.description = :fDescription where p.id = :fPostId
+            """)
+    int updatePost(@Param("fPostId") int id, @Param("fHeader") String header, @Param("fDescription") String description);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            update Post p set p.attachmentFile = ' '
+                        where p.id =:fPostId""")
+    int deleteAttachment(@Param("fPostId") int id);
+
+    @Modifying
+    @Query("""
+            delete Post p where p.id = :fPostId
+            """)
+    int deletePost(@Param("fPostId") int id);
+
+    @Query(value = """
+            SELECT * FROM posts p
+            WHERE p.j_user_id IN 
+                        (SELECT s.subscriber_id_to
+                                    FROM subscriptions s 
+                                    WHERE s.subscriber_id = :fId)
+            """, nativeQuery = true)
+    List<Post> findSubscribersPostsByUserId(int id);
 }
--- src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
@@ -1,7 +1,25 @@
 package ru.job4j.socialmedia.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.socialmedia.model.User;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("""
+            select u from User u 
+                        where u.username = :fUsername 
+                                    and u.password = :fPassword
+            """)
+    Optional<User> findUserByLoginAndPassword(@Param("fUsername") String username, @Param("fPassword") String password);
+
+    @Query(value = """
+            SELECT * FROM users u 
+                        JOIN subscriptions s ON u.id = s.subscriber_id
+                                   WHERE u.id = :fId
+            """, nativeQuery = true)
+    List<User> findSubscribersById(@Param("fId") int id);
 }
